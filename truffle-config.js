3ecc2ca84176e15e9b81469d50bd5545092a26ac
Joseph DeAngeloi <josephdeangeloi@Josephs-MBP.home>
2021-09-19 17:04:18 -0400

    All files commited

--- truffle-config.js
+++ truffle-config.js
@@ -1,9 +1,10 @@
 module.exports = {
   networks: {
-    local: {
-      host: "localhost",
-      port: 8545,
-      network_id: "*" // Match any network id
+    development: {
+        host: 'localhost',
+        port: 8545,
+        network_id: '*'
     }
   }
 };
+
